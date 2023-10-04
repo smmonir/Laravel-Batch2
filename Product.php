2d008103a4a7ffe29956149dc54f731c652923ff
Monir Uddin <eng.smmonir@gmail.com>
2023-10-04 22:17:31 +0600

    Lab Test 4

---
+++ Product.php
@@ -0,0 +1,25 @@
+<?php
+class Product {
+    public $id;
+	public $name;
+	public $price;
+    
+    public function __construct($id, $name, $price) {
+        $this->id=$id;
+		$this->name=$name;
+		$this->price=$price;
+    }
+
+    public function getFormattedPrice($price){
+	  return number_format((float)$price, 2, '.', '');
+	}
+
+    public function showDetails(){
+	 echo "ID: ".$this->id."\n";
+	 echo "Name: ".$this->name."\n";
+	 echo "Price: ".$this->getFormattedPrice($this->price);
+	}
+}
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+?>
